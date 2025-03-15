603b87735a4914127e2c384379aa50a4193e4772
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-03-15 19:18:27 +0300

    Задача 2.5.6.2 первая проверка

--- src/Main.java
+++ src/Main.java
@@ -9,33 +9,33 @@ public class Main {
         System.out.println(s.charAt(0));
         System.out.println(s.subSequence(0, 3));
         System.out.println(s.toString());
+    }
+
+    public static class AsciiCharSequence implements CharSequence {
+        byte[] bytes;
+
+        AsciiCharSequence(byte[] bytes) {
+            this.bytes = bytes;
         }
 
-        public static class AsciiCharSequence implements CharSequence {
-            byte[] bytes;
+        @Override
+        public int length() {
+            return bytes.length;
+        }
 
-            AsciiCharSequence(byte[] bytes) {
-                this.bytes = bytes;
-            }
+        @Override
+        public char charAt(int index) {
+            return (char) bytes[index];
+        }
 
-            @Override
-            public int length() {
-                return bytes.length;
-            }
+        @Override
+        public CharSequence subSequence(int start, int end) {
+            return new AsciiCharSequence(Arrays.copyOfRange(bytes, start, end));
+        }
 
-            @Override
-            public char charAt(int index) {
-                return (char) bytes[index];
-            }
-
-            @Override
-            public CharSequence subSequence(int start, int end) {
-                return new AsciiCharSequence(Arrays.copyOfRange(bytes, start, end));
-            }
-
-            @Override
-            public String toString() {
-                return new String(bytes);
-            }
+        @Override
+        public String toString() {
+            return new String(bytes);
         }
     }
+}
